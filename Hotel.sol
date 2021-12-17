dc74fc00eb997781f39f93c2e046021867810abb
Nazanin Goudarzi <31389568+nazaninGoudarzi@users.noreply.github.com>
2021-12-18 00:09:55 +0330

    Update Hotel.sol

--- Hotel.sol
+++ Hotel.sol
@@ -9,39 +9,49 @@ contract Hotel {
         //if a room is full, this will be true
         bool full;
     }
+    struct customer {
+        uint numOfnights;
+        uint keyCode;
+    }
     mapping (uint => room) singleRoom;
     mapping (uint => room) doubleRoom;
-    mapping (address => uint) numOfnights;
+    mapping (address => customer) registration;
     uint constant public singleRoomCost = 100;
     uint constant public doubleRoomCost = 150;
     uint numOfSingle;
     uint numOfDouble;
     uint fullSingleRoom = 0;
     uint fullDoubleRoom = 0;
+    uint numOfCustomers = 0;
     
     // initiating the owner of the hotel
     constructor(uint single , uint double) {
         owner = msg.sender;
         numOfSingle = single;
         numOfDouble = double;
+        for (uint i = 0; i < numOfSingle; i++) 
+            singleRoom[i].full = false;
+        for (uint i = 0; i < numOfDouble; i++) 
+            doubleRoom[i].full = false;
     }
 
     //renting function
     function rent (string memory typeR , uint nights) external payable {
         if (keccak256(abi.encodePacked(typeR)) == keccak256(abi.encodePacked("Single"))) {
             require (fullSingleRoom != numOfSingle , 'We do not have any vacant single room!');
-            for (uint i = 0; i < numOfSingle; i++){
+            for (uint i = 0; i < numOfSingle; i++) {
                 if(singleRoom[i].full == false) {
                     require (msg.value >= (nights*singleRoomCost),'Not enough money!');
                     if(msg.value >= (nights*singleRoomCost)){
-                        numOfnights[msg.sender] = nights;
+                        registration[msg.sender].numOfnights = nights;
+                        registration[msg.sender].keyCode = convert((msg.sender).codehash);
                         singleRoom[i].full = true;
-                        /////refundd msg.value - (nights*singleRoomCost)
+                        fullSingleRoom ++;
+                        address payable customerAddress = payable(msg.sender);
+                        customerAddress.transfer(msg.value - (nights*singleRoomCost));
+                        numOfCustomers ++;
                         break;
                     }
-                }
-                else if (singleRoom[i].full == true) {
-                    fullSingleRoom ++;
                 }
             }
         }
@@ -51,15 +61,41 @@ contract Hotel {
                 if(doubleRoom[i].full == false) {
                     require (msg.value >= (nights*doubleRoomCost),'Not enough money!');
                     if(msg.value >= (nights*doubleRoomCost)){
-                        numOfnights[msg.sender] = nights;
+                        registration[msg.sender].numOfnights = nights;
+                        registration[msg.sender].keyCode = convert((msg.sender).codehash);
                         doubleRoom[i].full = true;
-                        /////refundd msg.value - (nights*singleRoomCost)
+                        fullDoubleRoom ++;
+                        address payable customerAddress = payable(msg.sender);
+                        customerAddress.transfer(msg.value - (nights*doubleRoomCost));
+                        numOfCustomers ++;
                         break;
                     }
                 }
-                else if (doubleRoom[i].full == true) {
-                    fullDoubleRoom ++;
-                }
+            }
+        }
+    }
+
+    //converting function from bytes32 to uint256
+    function convert (bytes32 x) public pure returns (uint256) {
+        uint256 y;
+        for (uint256 i = 0; i < 32; i++) {
+            uint256 c = (uint256(x) >> (i * 8)) & 0xff;
+            if (48 <= c && c <= 57)
+                y += (c - 48) * 10 ** i;
+            else
+                break;
+        }
+        return y;
+    }
+
+    // welcome event
+    event welcome (string message);
+    //entring the room
+    function entring () public {
+        for (uint i = 0; i < numOfCustomers; i++) {
+            require(registration[i].keyCode == convert((msg.sender).codehash),'You sould go to the hotel reception!');
+            if(registration[i].keyCode == uint256(convert((msg.sender).codehash))) {
+                emit welcome ("Welcome to your room.");
             }
         }
     }
